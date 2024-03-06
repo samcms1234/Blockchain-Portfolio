c342e3caf3e366d6503b36c0d5a984f8a9e41e47
Saumya Srivastava <samdevpro99@gmail.com>
2024-03-06 23:02:20 +0530

    first commit

---
+++ client/src/components/utils/contracts-config.js
@@ -0,0 +1,5 @@
+
+      export const contractAddress = "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318"
+      export const ownerAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+      export const networkDeployedTo = "localhost"
+    
