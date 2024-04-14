4652d18360c258044c00c9e996600774eb74e202
Saumya Srivastava <samdevpro99@gmail.com>
2024-04-15 02:22:21 +0530

    Smart contract improved

--- contracts/Portfolio.sol
+++ contracts/Portfolio.sol
@@ -1,111 +1,153 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.7.0 <0.9.0;
+// SPDX-License-Identifier: MIT
 
-contract Portfolio{
- 
-  struct Project{
-      uint id;
-      string name;
-      string description;
-      string image;
-      string githubLink;
-  }
+pragma solidity ^0.8.0;
 
-  struct Education{
-      uint id;
-      string date;
-      string degree;
-      string knowledgeAcquired;
-      string instutionName;
-  }
-  //check expreinece code with your solution
-  struct Experience{
-      uint id;
-      string date;
-      string post;
-      string knowledgeAcquired;
-      string compamyName;
-  }
-  Project[3] public projects;
-  Education[3] public educationDetails;
-  Experience[3] public experienceDetails;
+contract Portfolio {
+    
+    struct Education {
+        uint index;
+        string collegeName;
+        uint startingYear;
+        uint endYear;
+        string degree;
+        string[] skillsAcquired;
+        string description;
+    }
 
-  string public imageLink="Add your cid";
-  string public description="over 6 months of practical experience with a good knowledge in blockchain development.i help web3 community by contributing in the web3 space.";
-  string public resumeLink="Add your cid";
-  uint projectCount;
-  uint educationCount;
-  uint experienceCount;
-  address public manager;
+    struct Experience {
+        uint index;
+        string companyName;
+        string position;
+        string startingTime;
+        string endTime;
+        string[] knowledgeAcquired;
+        string description;
+    }
 
-  constructor(){
-      manager=msg.sender;
-  }
+    struct Project {
+        uint index;
+        string title;
+        string githubLink;
+        string description;
+        string skillsAcquired;
+        string[] images;
+    }
 
-  modifier onlyManager(){
-      require(manager==msg.sender,"You are not the manager");
-      _;
-  }
-  
-  //Project
-  function insertProject(string calldata _name,string calldata _description,string calldata _image,string calldata _githubLink) external{
-      require(projectCount<3,"Only 3 projects allowed");
-      projects[projectCount] = Project(projectCount,_name,_description,_image,_githubLink);
-      projectCount++;
-  }
+    uint public noOfEducation;
+    uint public noOfProjects;
+    uint public noOfExperience;
 
-  function changeProject(string calldata _name,string calldata _description,string calldata _image,string calldata _githubLink,uint _projectCount) external{
-       require(_projectCount>=0 && _projectCount<3,"Only 3 projects allowed");
-      projects[projectCount] = Project(_projectCount,_name,_description,_image,_githubLink);
-  }
-    function allProjects() external view returns(Project[3] memory){
+    Experience[] public experiences;
+    Project[] public projects;
+    Education[] public educations;
+
+    address public manager;
+
+    constructor() {
+        manager = msg.sender;
+    }
+
+    modifier _onlyManager() {
+        require(msg.sender == manager, "Only manager can call this function");
+        _;
+    }
+
+    function addEduction(
+        string memory collegeName,
+        uint startingYear,
+        uint endYear,
+        string memory degree,
+        string[] memory skillsAcquired,
+        string memory description
+        ) _onlyManager external {
+        educations.push(Education(noOfEducation,collegeName,startingYear,endYear,degree, skillsAcquired, description));
+        noOfEducation++;
+    }
+
+    function getEducation(uint _i) public view returns(Education memory) {
+        return educations[_i];
+    }
+
+    function editEducation(
+        uint _i,
+        string memory collegeName,
+        uint startingYear,
+        uint endYear,
+        string memory degree,
+        string[] memory skillsAcquired,
+        string memory description
+        ) _onlyManager external {
+        Education memory education = Education(_i, collegeName,startingYear,endYear,degree, skillsAcquired, description);
+        educations[_i] = education;
+    }
+
+    function addProject(
+        string memory title,
+        string memory githubLink,
+        string memory description,
+        string memory skillsAcquired,
+        string[] memory images
+    ) _onlyManager external {
+        projects.push(Project(noOfProjects,title,githubLink,description,skillsAcquired, images));
+        noOfProjects++;
+    }
+
+    function getProject(uint _i) public view returns(Project memory) {
+        return projects[_i];
+    }
+
+    function editProject(
+        uint _i,
+        string memory title,
+        string memory githubLink,
+        string memory description,
+        string memory skillsAcquired,
+        string[] memory images
+    ) _onlyManager external {
+        Project memory project = Project(_i, title, githubLink, description, skillsAcquired, images);
+        projects[_i] = project;
+    }
+
+    function addExperience(
+        string memory companyName,
+        string memory position,
+        string memory startingTime,
+        string memory endTime,
+        string[] memory knowledgeAcquired,
+        string memory description
+    ) _onlyManager external {
+        experiences.push(Experience(noOfExperience, companyName, position, startingTime, endTime, knowledgeAcquired, description));
+        noOfExperience++;
+    }
+
+    function getExperience(uint _i) public view returns(Experience memory) {
+        return experiences[_i];
+    }
+
+    function editExperience(
+        uint _i,
+        string memory companyName,
+        string memory position,
+        string memory startingTime,
+        string memory endTime,
+        string[] memory knowledgeAcquired,
+        string memory description
+    ) _onlyManager external {
+        Experience memory experience = Experience(_i, companyName, position, startingTime, endTime, knowledgeAcquired, description);
+        experiences[_i] = experience;
+    }
+
+    function allProjects() external view returns(Project[] memory){
         return projects;
     }
-
-//Education
-   function insertEducation(string calldata _date,string calldata _degree,string calldata _knowledgeAcquired,string calldata _instutionName) external  onlyManager{
-     require(educationCount<3,"Only 3 education details allowed");
-     educationDetails[educationCount]=Education(educationCount,_date,_degree,_knowledgeAcquired,_instutionName);
-     educationCount++;
+    function allEductationDetails() external view returns(Education[] memory){
+        return educations;
+    }
+    function allExperienceDetails() external view returns(Experience[] memory){
+      return experiences;
     }
 
-  function changeEducation(string calldata _date,string calldata _degree,string calldata _knowledgeAcquired,string calldata _instutionName,uint _educationDetailCount) external onlyManager{
-      require(_educationDetailCount>=0 && _educationDetailCount<3,"Invalid educationCount");
-      educationDetails[_educationDetailCount]=Education(_educationDetailCount,_date,_degree,_knowledgeAcquired,_instutionName);
-  }
-
-    function allEductationDetails() external view returns(Education[3] memory){
-      return educationDetails;
-  }
-//Experience
-
-    function insertExperience(string calldata _date,string calldata _post,string calldata _knowledgeAcquired,string calldata _companyName) external  onlyManager{
-     require(experienceCount<3,"Only 3 education details allowed");
-     experienceDetails[experienceCount]=Experience(experienceCount,_date,_post,_knowledgeAcquired,_companyName);
-     experienceCount++;
+    function donate() public payable {
+        payable(manager).transfer(msg.value);
     }
-
-    function changeExperience(string calldata _date,string calldata _post,string calldata _knowledgeAcquired,string calldata _companyName,uint _experienceDetailCount) external  onlyManager{
-     require(_experienceDetailCount>=0 && _experienceDetailCount<3,"Invalid experienceCount");
-     experienceDetails[_experienceDetailCount]=Experience(_experienceDetailCount,_date,_post,_knowledgeAcquired,_companyName);
-    }
-    
-    function allExperienceDetails() external view returns(Experience[3] memory){
-      return experienceDetails;
-    }
-  function changeDescription(string calldata _description) external{
-      description=_description;
-  }
-
-    function changeResumeLink(string calldata _resumeLink) external onlyManager{
-      resumeLink=_resumeLink;
-  }
-     function changeImageLink(string calldata _imageLink) external onlyManager{
-      imageLink=_imageLink;
-  }
-
-  function donate() public payable{
-      payable(manager).transfer(msg.value);
-  }
-
 }
